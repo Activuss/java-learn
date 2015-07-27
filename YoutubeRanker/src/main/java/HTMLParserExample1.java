6aaec2dd9023f4cec0a50666c4fefe0837a483d9
Activuss <activuss@gmail.com>
2015-07-27 07:57:28 +0300

    test html parsing library

---
+++ YoutubeRanker/src/main/java/HTMLParserExample1.java
@@ -0,0 +1,41 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HTMLParserExample1 {
+
+    public static void main(String[] args) {
+
+        Document doc;
+        try {
+
+            // need http protocol
+            doc = Jsoup.connect("https://www.youtube.com/watch?v=E1tjmHYUHb4")
+                    .userAgent("Mozilla")
+                    .get();
+
+            // get page title
+            String title = doc.title();
+            System.out.println("title : " + title);
+
+            System.out.println("Watches: " + doc.getElementsByClass("watch-view-count").text());
+
+            // get all links
+            Elements links = doc.select(".content-wrapper .yt-uix-sessionlink");
+            for (Element link : links) {
+
+                System.out.println("\nlink : " + link.attr("href"));
+                System.out.println("text : " + link.text());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
